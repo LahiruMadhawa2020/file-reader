79ef616a955d8edd6f754a89eb80b7e8ba234659
Lahiru Madhawa <lmadhawa@wiley.com>
2022-01-02 19:24:10 +0530

    changed the classifiers in the setup.py file

--- setup.py
+++ setup.py
@@ -19,22 +19,28 @@ setup(
     author_email='kmlahiru24@gmail.com',
     description='file reader utility',
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/LahiruMadhawa2020/file-reader-tester.git',
-    packages=[''],
+    packages=['utility_file_reader'],
+    include_package_data=True,
     # install_requires=["setuptools==60.1.0", "wheel==0.37.1", "pytest==6.1.2", "pandas==1.1.4"],
-    python_requires=">=3.6",
+    python_requires='>=3.6',
+    license='MIT',
+    platforms=['any'],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Education',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         # Programming Languages Used..
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Database',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
